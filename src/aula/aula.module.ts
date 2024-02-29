d21e1c6877146dc3b5d66590ba6481f320dd84e4
Christopher Alexandre <christopher.alexandre.oliver@gmail.com>
2024-02-29 17:02:52 -0300

    fix: arquivo aula.module import aula.comtroller

--- src/aula/aula.module.ts
+++ src/aula/aula.module.ts
@@ -1,9 +1,10 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AulaService } from './aula.service';
 import { AulaController } from './aula.controller';
 
 @Module({
   providers: [AulaService],
-  controllers: [AulaController]
+  controllers: [AulaController],
 })
 export class AulaModule {}
